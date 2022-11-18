ddf2f4f75401f79af472b5b10819fb1fc196d84c
Andregit93 <andresevtian501@gmail.com>
2022-11-19 01:57:28 +0700

    menambah materi pengulangan

--- pertemuan3/latihan1.php
+++ pertemuan3/latihan1.php
@@ -32,16 +32,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>latihan1</title>
+    <style>
+        .warna {
+            background-color: salmon;
+        }
+    </style>
 </head>
 <body>
     <table border="1" cellpadding="10" cellspacing="0">
-        <?php for($i = 1; $i <= 3; $i++) : ?>
-            <tr>
+        <?php for($i = 1; $i <= 5; $i++) : ?>
+            <?php if($i %2 ==1) : ?>
+                <tr class="warna">
+            <?php else : ?>
+                <tr>
+            <?php endif; ?>
                 <?php for($j = 1; $j <= 5; $j++) : ?>
                     <td><?php echo "$i, $j"; ?></td>
-                <?php endfor ?>
+                <?php endfor; ?>
             </tr>
-        <?php endfor ?>
+        <?php endfor; ?>
     </table>
     
 </body>
---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,20 @@
+<?php 
+// pengkondisian / percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+
+$s = 30;
+if ($s < 20) {
+    echo "Benar";
+} elseif ($s == 20) {
+    echo "Bingo";
+} else {
+    echo "Salah";
+}
+
+
+
+?>
