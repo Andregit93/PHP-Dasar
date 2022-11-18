707936b6d612dc248b15253b87d58c14d0cc75f7
Andregit93 <andresevtian501@gmail.com>
2022-11-19 01:03:34 +0700

    pengulangan pada PHP

--- pertemuan3/latihan1.php
+++ pertemuan3/latihan1.php
@@ -17,14 +17,32 @@
 // $i++;
 // }
 
-$i = 10;
-do {
-    echo"Hello world! <br>";
-$i++;
-} while($i < 5);
-
-  
-  
-  
+// $i = 10;
+// do {
+//     echo"Hello world! <br>";
+// $i++;
+// } while($i < 5);
   
   ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan1</title>
+</head>
+<body>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php for($i = 1; $i <= 3; $i++) : ?>
+            <tr>
+                <?php for($j = 1; $j <= 5; $j++) : ?>
+                    <td><?php echo "$i, $j"; ?></td>
+                <?php endfor ?>
+            </tr>
+        <?php endfor ?>
+    </table>
+    
+</body>
+</html>
